ccd7702a997fb491f4406548b8d99b52f0a9e095
AlanTiberio <56177169+AlanTiberio@users.noreply.github.com>
2025-02-20 07:11:36 -0300

    01.Iniciando_Em_Python.py

--- aula8_exercicios.py
+++ aula8_exercicios.py
@@ -1,14 +1,24 @@
-nome = "Alan"
-sobrenome = "Tiberio"
-idade = 28
+# Importando a bliblioteca  para que possamos utilizar a data atual
+from datetime import date
+data = date.today()
+
+nome = "Carlitos"
+sobrenome = "Alburquerque"
 ano_nascimento = 1996
-maior_de_idade = idade >= 18
+idade = data.year - 1996
+maior_de_idade = ""
 altura_metros = 1.78
 
+#Verificando se é maior ou menor de idade
+if idade >= 18:
+    maior_de_idade == "Você é Maior de idade"
+else:    
+    maior_de_idade == "Você é Menor de idade"
+
+#Realizando a impressão dos resultados no terminal  
 print("Nome: ", nome)
 print("Sobrenome: ", sobrenome)
 print("Idade: ", idade)
 print("Ano de Nascimento: ", ano_nascimento)
 print("E maior de Idade?: ", maior_de_idade)
 print("Altura em Metros: ", altura_metros)
-
