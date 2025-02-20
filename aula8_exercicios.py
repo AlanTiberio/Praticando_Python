7314e9f8eeda710e70ffae78875b1a97dc57c34d
AlanTiberio <56177169+AlanTiberio@users.noreply.github.com>
2025-02-20 06:48:17 -0300

    Add files via upload

---
+++ aula8_exercicios.py
@@ -0,0 +1,14 @@
+nome = "Alan"
+sobrenome = "Tiberio"
+idade = 28
+ano_nascimento = 1996
+maior_de_idade = idade >= 18
+altura_metros = 1.78
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de Nascimento: ", ano_nascimento)
+print("E maior de Idade?: ", maior_de_idade)
+print("Altura em Metros: ", altura_metros)
+
